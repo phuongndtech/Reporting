6f4d9986b9339b63490106395c22621e44f9102f
PHUONGND_TECH <phuongnd@ichiba.vn>
2024-03-23 15:08:14 +0700

    add

--- src/Reporting.Infrastructure/obj/Debug/net8.0/Reporting.Infrastructure.AssemblyInfo.cs
+++ src/Reporting.Infrastructure/obj/Debug/net8.0/Reporting.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Reporting.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7acb65eb19bdbfa3ca22a4de24b2848f46ffa9ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Reporting.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Reporting.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
